e44c84db7dc1aff46e6b3559f8f05e3f1a7a2218
Muneerabbas <muneer.abbas9595@gmail.com>
2026-08-01 11:05:41 +0530

    smooth scrooll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -120,7 +120,7 @@ export default async function RootLayout({
         />
         <ProductsProvider products={products}>
         <CartProvider>
-          {/* <SmoothScroll /> */}
+          <SmoothScroll />
           <Analytics />
 
           <a
